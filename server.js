e2570957a88a36568ba0a93f142aaf3ec8ff431f
Monika Singla <monikasingla2605@github.com>
2019-02-01 06:56:38 -0800

    added routes

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 const app = express();
 
+const db = require('./config/keys').mongoURI;
+mongoose
+.connect(db)
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
 app.get('/', (req, res) => 
-  res.send('Hello'));
+  res.send('Hello World'));
+
+  //use routes
+  app.use('/api/users', users);
+  app.use('/api/profile', profile);
+  app.use('/api/posts', posts);
 
 const port = 5005;
 app.listen(port, () => 
